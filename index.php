263f979781dd34dfa49a9666e07f11d0087fd6e6
Dale Kanikkeberg <kanikkeberg.dale@student.greenriver.edu>
2024-05-16 01:37:39 -0400

    OOPify the Diner app

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Require the autoload file
+// Require the necessary files
 require_once ('vendor/autoload.php');
 
 // Instantiate the F3 Base class
@@ -42,32 +42,74 @@ $f3->route('GET /menus/dinner', function() {
 
 // Order form part 1
 $f3->route('GET|POST /order1', function($f3) {
+    $food = "";
+    $meal = "";
+
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "<p>you got here using POST</p>";
 
         // Get the data from the POST array
-        $food = $_POST['food'];
-        $meal = $_POST['meal'];
-
-
-        // If the data is valid
-        if (true) {
-            $f3->set('SESSION.food', $food);
-            $f3->set('SESSION.meal', $meal);
+        if (Validate::validFood($_POST['food'])){
+            $food = $_POST['food'];
+        } else {
+            $f3->set('errors["food"]', 'Please enter a food');
         }
-    } else {
-        echo "<p>you got here using GET</p>";
+        if (isset($_POST['meal']) and Validate::validMeal($_POST['meal'])){
+            $meal = $_POST['meal'];
+        } else {
+            $meal = "Lunch";
+        }
+
+        $meal = isset($_POST['meal']) ? $_POST["meal"] : "";
+
+        // Add the data to the session array
+        $order = new Order($food, $meal);
+        $f3->set('SESSION.order', $order);
+
+        // If there are no errors, send the user to the next form
+        if (empty($f3->get('errors'))) {
+            $f3->reroute("order2");
+        }
     }
+
+    $meals = DataLayer::getMeals();
+    $f3->set('meals', $meals);
+
     $view = new Template();
     echo $view->render('views/order1.html');
 });
 
 // Order form part 2
-$f3->route('GET|POST /order2', function() {
+$f3->route('GET|POST /order2', function($f3) {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        if (isset($_POST["conds"])) {
+            $condiments = implode($_POST["conds"]);
+        }
+        if (true) {
+            $f3->get('SESSION.order')->setCondiments($condiments);
+
+            // Send the user to the next page
+            $f3->reroute("order-summary");
+        }
+    } else {
+
+    }
+    $condiments = DataLayer::getCondiments();
+    $f3->set('condiments', $condiments);
+    
     // Render a view page
     $view = new Template();
     echo $view->render('views/order2.html');
 });
+
+// Define an order-summary route
+$f3->route('GET /order-summary', function($f3) {
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order-summary.html');
+    session_destroy();
+});
+
 
 // Run Fat-Free
 $f3->run();
---
+++ model/data-layer.php
@@ -0,0 +1,19 @@
+<?php
+
+/* This is my Data Layer
+ * It belongs to the Model
+ * */
+
+class DataLayer
+{
+// Get the meals for the Diner app
+    static function getMeals()
+    {
+        return array('breakfast', 'brunch', 'lunch', 'dinner', 'dessert');
+    }
+
+    static function getCondiments()
+    {
+        return array('ketchup', 'mustard', 'sriracha', 'kim chi', 'sour cream');
+    }
+}
